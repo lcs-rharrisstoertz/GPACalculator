5aab23b45fa3e31df232b477a0ad138432039a6a
Rowan Harris-Stoertz <rharrisstoertz@lcs.on.ca>
2022-03-25 11:19:47 -0400

    Organized project files, added Course structure

---
+++ Shared/Model/Course.swift
@@ -0,0 +1,14 @@
+//
+//  Course.swift
+//  GPACalculator
+//
+//  Created by Harris-Stoertz, Rowan on 2022-03-25.
+//
+
+import Foundation
+
+struct Task {
+    var honors: Bool
+    var courseName: String
+    var grade: Double
+}
