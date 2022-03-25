e77ea252a5e966cfe83f625df064673a136c7eb7
Rowan Harris-Stoertz <rharrisstoertz@lcs.on.ca>
2022-03-25 11:12:07 -0400

    Initial Commit

---
+++ Shared/GPACalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  GPACalculatorApp.swift
+//  Shared
+//
+//  Created by Harris-Stoertz, Rowan on 2022-03-25.
+//
+
+import SwiftUI
+
+@main
+struct GPACalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
