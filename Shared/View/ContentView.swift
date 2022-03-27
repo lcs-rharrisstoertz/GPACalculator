31bde875c49fd77229dba6f2ccb61910c650bc88
Rowan Harris-Stoertz <rharrisstoertz@lcs.on.ca>
2022-03-27 16:44:33 -0400

    Switched picker for whether a course is AP to a toggle button

--- Shared/View/ContentView.swift
+++ Shared/View/ContentView.swift
@@ -13,24 +13,18 @@ struct ContentView: View {
     @State private var newCourseGrade = ""
     @State private var newCourseHonors = false
     var body: some View {
-        Form {
+        List {
             ForEach (courseList.courses) { Course in
                 CourseView(Course: Course)
             }
-            HStack {
-                TextField ("Grade", value: $newCourseGrade, formatter: NumberFormatter())
-                    .keyboardType(.decimalPad)
-                TextField ("Course name", text: $newCourseName)
-                VStack {
-                    Text("AP/Honors")
-                    Picker ("AP/Honors", selection: $newCourseHonors) {
-                        Text("Yes").tag(true)
-                        Text("No").tag(false)
-                    }.pickerStyle(SegmentedPickerStyle())
-                }
-                Button("+") {
-                    courseList.courses.append(Course(honors: newCourseHonors, courseName: newCourseName, grade: Double(newCourseGrade)!))
-                }
+        }
+        HStack {
+            TextField ("Grade", value: $newCourseGrade, formatter: NumberFormatter())
+                .keyboardType(.decimalPad)
+            TextField ("Course name", text: $newCourseName)
+            Toggle("AP/Honors", isOn: $newCourseHonors)
+            Button("+") {
+                courseList.courses.append(Course(honors: newCourseHonors, courseName: newCourseName, grade: Double(newCourseGrade)!))
             }
         }
     }
